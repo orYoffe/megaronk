906c5145c67e9fdb336ef3bf09dd9b9d4455139f
Or Yoffe <orYoffe@users.noreply.github.com>
2020-08-08 17:42:14 +0200

    added megaronk internals

---
+++ index.js
@@ -0,0 +1,8 @@
+throw new Error(`
+megaronk, made to sound like Mega Wrong, is a library meant to tell developers that a company is bad in the Job description.
+This library should warn developers from a bad position/workplace by reading in the job description or being asked about it in an interview.
+If the recruiters will search and find this library they would probably not jump into the source code and try to run this library. 
+Developers out of curiosity will go and search and hopefully test and see this message.
+It is just an idea to help developers, please don't get angry at me in the issues section :)
+Please don't put this in production
+`)
